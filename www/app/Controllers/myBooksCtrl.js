85df418202fef1be618009dacd21ff87d0c4b9d2
tahasabih <taha.sabih@outlook.com>
2015-05-20 22:19:29 +0800

    some changes

--- www/app/Controllers/myBooksCtrl.js
+++ www/app/Controllers/myBooksCtrl.js
@@ -5,7 +5,7 @@
 	angular.module('BND').controller('myBooksCtrl', ['$scope', '$http', '$ionicModal', 'HttpGetApi', myBooksCtrl])
 
 	function myBooksCtrl($scope, $http, $ionicModal, HttpGetApi) {
-	  	HttpGetApi.getBooksList(function (data) {
+	  	HttpGetApi.getBooksList().then(function (data) {
 	  		$scope.books = data.items;
 	  	});
 	  	
--- www/app/Services/GETS/HttpGetApi.js
+++ www/app/Services/GETS/HttpGetApi.js
@@ -1,18 +1,23 @@
 (function () {
 	'use strict';
 
-	angular.module('BND').factory('HttpGetApi', ['$http', HttpGetApi]);
+	angular.module('BND').factory('HttpGetApi', ['$http', "$q", "$ionicLoading", HttpGetApi]);
 
-	function HttpGetApi($http) {
+	function HttpGetApi($http, $q, $ionicLoading) {
 		var myBooks = []
-		function getBooksList(callback) {
+		$ionicLoading.show({ template: "Fetching..."})
+		function getBooksList() {
+			var deferred = $q.defer();
 	    	$http({method : 'GET',url : 'https://www.googleapis.com/books/v1/volumes?q=inauthor:"J. K. Rowling"&maxResults=20'})
 	        .success(function(data, status) {
-	            callback(data);
+	            deferred.resolve(data)
+	            $ionicLoading.hide();
 	        })
 	        .error(function(data, status) {
 	            alert("Error");
+	            deferred.reject();
 	        });
+	        return deferred.promise;
 	    }
 
 	    return {
